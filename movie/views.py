b4572f10f1defc39a2bf7ee9f53a42b6032e7601
adrianfhr <adrianfahriaffandi@gmail.com>
2024-06-18 19:46:11 +0700

    trigger deployment build

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18185869a4bce154e94a8ae802a68df170974e3667b1a7571b338829860ac523
+oid sha256:8789dae8d9186a46440e3f67fa91e9dba8db706871c385a7e0ddf2c70f6a92dd
 size 27551
