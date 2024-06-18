5f94b29e127822252e6bf3a9a27a1509ae594e52
adrianfhr <adrianfahriaffandi@gmail.com>
2024-06-18 20:22:00 +0700

    feat: home endpoint

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8789dae8d9186a46440e3f67fa91e9dba8db706871c385a7e0ddf2c70f6a92dd
-size 27551
+oid sha256:97ef86f63a9e05ad9b632d1215379833f7a8473d93d9b381a1ca5fe5aa9a2484
+size 27679
