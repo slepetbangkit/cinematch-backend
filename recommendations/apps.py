85b1e1ee0d124cbe1768894c2b537de451fd55bd
sulthanftr <sulthanftr@gmail.com>
2024-06-19 18:30:59 +0700

    refactor: download zipped model files from bucket

--- recommendations/apps.py
+++ recommendations/apps.py
@@ -11,34 +11,43 @@ class RecommendationsConfig(AppConfig):
 
     def ready(self):
         import pickle
+        from google.cloud import storage
 
         print('\n[RECOMMENDATIONS MODEL]')
         path = 'recommendations/similarity_model'
-        file_path = f'{path}/similarity.pkl'
-        if os.path.isfile(file_path):
-            print(f"{file_path} FOUND!")
+
+        if os.path.isdir(f"{path}/"):
+            print("Directory FOUND!")
         else:
-            print(f"{file_path} NOT FOUND!")
+            print("Directory NOT FOUND!")
 
-            bucket_name = settings.GS_BUCKET_NAME
-            object_path = f'{bucket_name}/models/similarity/similarity_model/similarity.pkl'
-            url = f"https://storage.googleapis.com/{object_path}"
+            if not os.path.isfile(f"{path}.zip"):
+                bucket_name = settings.GS_BUCKET_NAME
+                object_path = f'{bucket_name}/models/similarity/similarity_model.zip'
+                url = f"https://storage.googleapis.com/{object_path}"
 
-            print("Downloading from bucket...\n", url, "\n")
-            urllib.request.urlretrieve(url, file_path)
-            print(
-                "Downloaded object {} from bucket {} to local file {}."
-                .format(
-                    'similarity_model.zip', bucket_name, file_path
+                print("Downloading from bucket...\n", url, "\n")
+                urllib.request.urlretrieve(url, f"{path}.zip")
+                print(
+                    "Downloaded object {} from bucket {} to local file {}."
+                    .format(
+                        'similarity_model.zip', bucket_name, f'{path}.zip'
+                    )
                 )
-            )
+            print("\nExtracting similarity_model.zip file...")
+            with zipfile.ZipFile(f'{path}.zip', 'r') as zip_ref:
+                zip_ref.extractall(f'{path}/')
+            os.remove(f'{path}.zip')
+
         loaded_data = {}
         for filename in os.listdir(f"{path}/"):
             filepath = os.path.join(path, filename)
             with open(filepath, 'rb') as file:
                 loaded_data[filename[:-4]] = pickle.load(file)
-            print(f"--{filepath} loaded")
+            print(f"--{filename} loaded")
         print()
+
         self.movies = loaded_data["movies"]
         self.features = loaded_data["features"]
         self.similarity = loaded_data["similarity"]
+
